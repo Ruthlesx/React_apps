ea39add75e59f606adbe00dc56d7e1fde8267ef6
Ruthlesx <ifeoramicheal@gmail.com>
2024-08-22 02:11:50 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Todo from './todo.js' 
+//import Todo from './todo.js'
+import Calculator from './Calculator';
+import Main from './Main' 
 
-
+/*
 function App() {
   return (
     <>
-    <Todo />
+    <Todo /> 
     
     </>
   )
-}
+} */
+
+  function App() {
+    return (
+      <>
+       <Calculator />
+      </>
+    )
+  }
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { useEffect, useState } from 'react'
+import React from 'react'
+
+const Main = () => {
+
+  const [items, setItems] = useState([])
+
+
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then((res) => {
+      setItems(res.data.meals)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+
+  const itemsList = items.map(({strMeal, strMealThumb, idMeal}) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb}  />
+
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+         
+      </section>
+    )
+  })
+
+  return <div className="items-container">{itemsList}</div>
+}
+
+export default Main
